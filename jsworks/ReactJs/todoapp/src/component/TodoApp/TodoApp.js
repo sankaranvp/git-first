588a98a7787498e34fceb2bd095d9041499467eb
sankaranvp <sknvp2010@gmail.com>
2020-12-10 12:01:11 +0530

    Small modifications in Todo App js & css

--- jsworks/ReactJs/todoapp/src/component/TodoApp/TodoApp.js
+++ jsworks/ReactJs/todoapp/src/component/TodoApp/TodoApp.js
@@ -4,51 +4,75 @@ import "./TodoApp.css"
 class TodoApp extends Component {
 
 	state = {
-			input : "",
-			items : []
+			input    : "",
+			items    : [],
+			isEdit   : false,
+			edId     : -1
 	};
 
+	/* Make input value */
 	handleChange=(event)=>{		
 		this.setState({
 			input : event.target.value
 		});		
 	};	
 
+	/* Store value when click enter */
 	storeItems=(event)=>{
-		event.preventDefault();
-		/*const allItems = this.state.items;    //Usual method
-		allItems.push(this.state.input);        //Usual method */
+		event.preventDefault();	
+		
+		/* Update existing value */			
+		if(this.state.isEdit){			 
+			this.state.items.splice(this.state.edId, 1, this.state.input);
+		    this.setState({ 
+		    	items : [...this.state.items]
+		    });
+		 /* Add new value */
+		}else{ 	
+			/*const allItems = this.state.items;    //Usual method
+			allItems.push(this.state.input);        //Usual method */
+			this.setState({
+				/*items : allItems      //Usual method */
+				items : [...this.state.items, this.state.input]   //Advanced method using Spread Operator
+			});
+		}
 
-		this.setState({
-			/*items : allItems      //Usual method */
-			items : [...this.state.items, this.state.input],   //Advanced method using Spread Operator
-			input : ""
+		/* Reset the input & boolean value */
+		this.setState({ 		    	
+	    	input : "",
+	    	isEdit : false
 		});
 	};
 
+	/* Delete item */
 	deleteItem=(id)=>{
 		/*const allItems = this.state.items; //Usual method
 		allItems.splice(index, 1);         //Usual method */
-
 		this.setState({
 			/*items : allItems          //Usual method */
 			items : this.state.items.filter((data, index) => index !== id) //Advanced method using filter
-		})
+		});
 	};
 
-	editItem=(id)=>{
-		alert("Edit here...");
-	};
+	/* Edit value when click  */
+	editItem=(id)=>{		
+		this.setState({ 
+			isEdit : true,
+			edId   : id,
+			input  : this.state.items[id]
+		});		
+	};	
 
 	render (){
 
-		const { input, items } = this.state;
+		const { input, items, edId } = this.state;		
 
 		return(
 			<div className="todo-container">	
 				<form className="input-section" onSubmit={this.storeItems} >
 					<h1>Todo App</h1>
 					<input type="text" value={input} onChange={this.handleChange} placeholder="Enter item here..." />
+					<input type="hidden" value={edId} readOnly />
 				</form>
 				<ul>
 					{items.map( (data, index)=>(
